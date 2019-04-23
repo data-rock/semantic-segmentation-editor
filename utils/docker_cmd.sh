4da2ccd6933c1c5d8916e910cd412fd69a0fff74
Duy Tin Truong <dttruong-dius@dius.com.au>
2019-04-23 15:45:05 +1000

    clean up and set some env variables

--- utils/docker_cmd.sh
+++ utils/docker_cmd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /code
-s3fs S3_BUCKET /root/images -o passwd_file=/root/.passwd-s3fs
-meteor --allow-superuser npm start
+echo ${AWS_ACCESS_KEY}:${AWS_SECRET_KEY} > /root/.passwd-s3fs && chmod 600 /root/.passwd-s3fs
+s3fs ${S3_BUCKET} /root/images -o passwd_file=/root/.passwd-s3fs
+meteor --allow-superuser npm start # this runs meteor in debug mode, see meteor build for production
