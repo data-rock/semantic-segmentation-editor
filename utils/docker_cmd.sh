7f65098867a0fbed9ded9f562fd0db948e87249b
Duy Tin Truong <dttruong-dius@dius.com.au>
2019-04-24 12:50:29 +1000

    add aws region

--- utils/docker_cmd.sh
+++ utils/docker_cmd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /code
 echo ${AWS_ACCESS_KEY}:${AWS_SECRET_KEY} > /root/.passwd-s3fs && chmod 600 /root/.passwd-s3fs
-s3fs ${S3_BUCKET} /root/images -o passwd_file=/root/.passwd-s3fs
+export AWS_REGION=${AWS_REGION:-ap-southeast-2}
+s3fs -o passwd_file=/root/.passwd-s3fs -o url=https://s3-${AWS_REGION}.amazonaws.com ${S3_BUCKET} /root/images
 
 # this runs meteor in debug mode, see meteor build for production
 # meteor --allow-superuser npm start
